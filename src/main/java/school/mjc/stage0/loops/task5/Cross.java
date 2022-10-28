91bcdee812ceb14fdfb7a6620ce13964f6728069
EDyugaeva <ed2408@yandex.ru>
2022-10-29 00:57:44 +0300

    commit

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -3,21 +3,10 @@ package school.mjc.stage0.loops.task5;
 public class Cross {
     public static void printCross(int sideLength) {
         int centre = sideLength / 2;
-//        for (int i = 0; i < sideLength; i++) {
-//            for (int j = 0; j < sideLength; j++) {
-//                if (j == centre || i == centre) {
-//                    System.out.print("8");
-//                    continue;
-//                }
-//                System.out.print(" ");
-//
-//            }
-//            System.out.println("");
-//
-//        }
-        for (int i = 0; i < centre; i++) {
+
+        for (int i = 0; i < sideLength; i++) {
             for (int j = 0; j < sideLength; j++) {
-                if (j == centre || i == centre || i == 0) {
+                if (j == centre || i == centre ) {
                     System.out.print("8");
                     continue;
                 }
@@ -31,6 +20,6 @@ public class Cross {
     }
 
     public static void main(String[] args) {
-        printCross(6);
+        printCross(5);
     }
 }
